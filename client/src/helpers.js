ca8252dcd3f0d007d949b48dbcbfedd08c887873
Jesse Nicholas <bigjmn@gmail.com>
2022-01-07 12:51:52 -0500

    debounce heloer

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -7,3 +7,15 @@ export function hideword(w){
   var newword = w.replace(regex, '_')
   return newword
 }
+
+export function debounce(func, delay){
+      let timer;
+      return function () {     //anonymous function
+        const context = this;
+	const args = arguments;
+	clearTimeout(timer);
+	timer = setTimeout(()=> {
+	    func.apply(context, args)
+	},delay);
+       }
+}
